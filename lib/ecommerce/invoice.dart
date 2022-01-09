cba0e100d9cb9e46516a548d93a2833b0517cfe3
https://github.com/zohirulhaque <zhremon@outlook.com>
2022-01-09 13:27:19 +0600

    Ecommerce app modified for submission 09-1-2022

--- lib/ecommerce/invoice.dart
+++ lib/ecommerce/invoice.dart
@@ -1,3 +1,247 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:signup_flutter_app/modelClass/productListmodel.dart';
+
+
+class Invoice extends StatefulWidget {
+  final productListModel DetailsView;
+  int orderNo;
+
+  Invoice(this.DetailsView, this.orderNo);
+
+  @override
+  State<Invoice> createState() => _InvoiceState();
+}
+
+class _InvoiceState extends State<Invoice> {
+  String name = '';
+  String mobile = '';
+  String address = '';
+  String pass = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getShapref();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        /*brightness: Brightness.dark,
+        backgroundColor: Colors.transparent,*/
+        elevation: 0.0,
+        toolbarHeight: 70,
+        title: Text("Your invoice"),
+        centerTitle: true,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20)),
+              gradient: LinearGradient(
+                  colors: [Colors.lightBlueAccent],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter)),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 4,
+              color: Colors.white70,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 18, right: 18),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Center(
+                        child: Text('Invoice no : 1000',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 20))),
+                    Divider(thickness: 1, color: Colors.white),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Name : $name',
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                          textAlign: TextAlign.left,
+                        )),
+                    Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Mobile : $mobile',
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                          textAlign: TextAlign.left,
+                        )),
+                    Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Address : $address',
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                          textAlign: TextAlign.left,
+                        )),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                height: 100,
+                color: Colors.white70,
+                child: ListTile(
+                  leading: Image.network(
+                    '${widget.DetailsView.imageUrl}',
+                    fit: BoxFit.cover,
+                  ),
+                  title: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text('${widget.DetailsView.title}')),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.teal)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                              'Price of Product    :       ${widget.DetailsView.price}',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 16))),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                              'Number of order    :       ${widget.orderNo}',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 16))),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                              'Total Discount    :       ${widget.DetailsView.discount}',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 16))),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                              'Unit of Product     :   ${widget.DetailsView.unit}',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 16))),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Divider(thickness: 1, color: Colors.pink),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                              'Net Bill    :       ${(widget.DetailsView.price * widget.orderNo) - widget.DetailsView.discount}',
+                              style:
+                                  TextStyle(color: Colors.red, fontSize: 20))),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  height: 50,
+                  width: 180,
+                  decoration: BoxDecoration(color: Colors.blueAccent),
+                  child: TextButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Center(
+                                    child: Text(
+                                  "Thanks For Yo Yo",
+                                  style: TextStyle(fontSize: 25),
+                                )),
+                                backgroundColor: Colors.white,
+                                elevation: 5,
+                              );
+                            });
+
+                        //
+                      },
+                      child: Text('Payment',
+                          style: TextStyle(color: Colors.white, fontSize: 20))),
+                ),
+                Container(
+                  height: 50,
+                  width: 180,
+                  decoration: BoxDecoration(color: Colors.blueAccent),
+                  child: TextButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Center(
+                                    child: Text(
+                                  "Thanks For Yo Yo",
+                                  style: TextStyle(fontSize: 25),
+                                )),
+                                backgroundColor: Colors.white,
+                                elevation: 5,
+                              );
+                            });
+                        //
+                      },
+                      child: Text('Cash In Delivery',
+                          style: TextStyle(color: Colors.white, fontSize: 20))),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  getShapref() async {
+    final pref = await SharedPreferences.getInstance();
+
+    setState(() {
+      name = pref.getString('user_name')!;
+      mobile = pref.getString('mobile')!;
+      address = pref.getString('address')!;
+      pass = pref.getString('pass')!;
+    });
+  }
+}
+/*
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -54,4 +298,4 @@ class Invoice extends StatelessWidget {
       ),
     );
   }
-}
+}*/
--- lib/login.dart
+++ lib/login.dart
@@ -3,27 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:signup_flutter_app/ecommerce/Home.dart';
-import 'package:signup_flutter_app/profile.dart';
-import 'package:signup_flutter_app/signup.dart';
-
+import 'signup.dart';
 import 'forgotpass.dart';
 import 'main.dart';
 
 class login extends StatefulWidget {
-
   @override
   _loginState createState() => _loginState();
 }
 
 class _loginState extends State<login> {
-  TextEditingController userController=TextEditingController();
-
-  TextEditingController passController=TextEditingController();
-
-  String user= '';
-
+  TextEditingController userController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  String user = '';
   String pass = '';
+
+  String superuser = 'admin';
+  String superpass = 'admin';
+
   @override
   void initState() {
     // TODO: implement initState
@@ -35,79 +32,95 @@ class _loginState extends State<login> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login Page'),
+        title: Center(child: Text('Login Page')),
       ),
-      body:SafeArea (
+      body: SafeArea(
           child: Column(
-            children: [
-
-              SizedBox(height: 20,),
-
-              Container(
-                height: 200,
-                child: FlareActor(
-                  'animation/Teddy.flr',
-                  alignment: Alignment.center,
-                  fit: BoxFit.cover,
-                  animation: 'success',
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 200,
+            child: FlareActor(
+              'animation/Teddy.flr',
+              alignment: Alignment.center,
+              fit: BoxFit.cover,
+              animation: 'success',
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: userController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                  hintText: 'Enter your Name',
+                  icon: Icon(
+                    Icons.account_box_outlined,
+                    size: 50,
+                    color: Colors.amberAccent,
+                  )),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: passController,
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Enter your Password',
+                  icon: Icon(
+                    Icons.password,
+                    size: 50,
+                    color: Colors.indigoAccent,
+                  )),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ForgotPass()));
+                },
+                child: Text(
+                  'Forgot Password',
+                  style: GoogleFonts.rubik(
+                      textStyle: TextStyle(color: Color(0xFF30F30E)),
+                      fontSize: 20),
                 ),
               ),
-              SizedBox(height: 20,),
-
-
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller:  userController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'User Name',
-                      hintText: 'Enter your Name',
-                      icon: Icon(Icons.account_box_outlined, size: 50,color: Colors.amberAccent,)
-                  ),
-                ),
-              ),
-              SizedBox(height: 20,),
-
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller:  passController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      hintText: 'Enter your Password',
-                      icon: Icon(Icons.password,size: 50,color: Colors.indigoAccent,)
-                  ),
-                ),
-              ),
-              SizedBox(height: 20,),
-
-              Container(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: TextButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=> ForgotPass ()));
-                  },
-                    child: Text('Forgot Password',style: GoogleFonts.rubik(textStyle: TextStyle(color: Color(
-                        0xFF30F30E)),fontSize: 20),),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20,),
-
-              Container(
-                height: 50,
-                width: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Colors.indigoAccent,
-                ),
-                //
-                child: TextButton(onPressed: (){
-                  /*Fluttertoast.showToast(
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 50,
+            width: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.indigoAccent,
+            ),
+            //
+            child: TextButton(
+              onPressed: () {
+                /*Fluttertoast.showToast(
                       msg: userController.text+'\n'+passController.text,
 
                       toastLength:Toast.LENGTH_LONG,
@@ -115,65 +128,68 @@ class _loginState extends State<login> {
                       backgroundColor: Colors.yellow,
                       textColor: Colors.greenAccent
                   );*/
-                  if(userController.text.isEmpty ||
-                  passController.text.isEmpty){
-                    Fluttertoast.showToast(
-                            msg: 'Please enter ID and Password',
-
-                            toastLength:Toast.LENGTH_LONG,
-                            gravity:ToastGravity.BOTTOM,
-                            backgroundColor: Colors.yellow,
-                            textColor: Colors.greenAccent
-                        );
-                  } else if(userController.text == user && passController.text ==pass){
-                    Fluttertoast.showToast(
-                        msg: 'Login successful',
-
-                        toastLength:Toast.LENGTH_LONG,
-                        gravity:ToastGravity.BOTTOM,
-                        backgroundColor: Colors.yellow,
-                        textColor: Colors.greenAccent
-                    );
-                     Navigator.push(context, MaterialPageRoute(builder: (context) => MyHome()));
-                  } else{
-                    Fluttertoast.showToast(
-                        msg: 'Please enter valid ID and Password',
-
-                        toastLength:Toast.LENGTH_LONG,
-                        gravity:ToastGravity.BOTTOM,
-                        backgroundColor: Colors.yellow,
-                        textColor: Colors.greenAccent
-                    );
-                  }
-                },
-                  child: Text('Sing In',
-                    style: GoogleFonts.rubik(
-                        textStyle:
-                        TextStyle(color: Color(0xFFFFFFFF)),
-                        fontSize: 24),),
-                ),
+                if (userController.text.isEmpty ||
+                    passController.text.isEmpty) {
+                  Fluttertoast.showToast(
+                      msg: 'Please enter ID and Password',
+                      toastLength: Toast.LENGTH_LONG,
+                      gravity: ToastGravity.BOTTOM,
+                      backgroundColor: Colors.yellow,
+                      textColor: Colors.greenAccent);
+                } else if((userController.text == user &&
+                    passController.text == pass) || ((userController.text == superuser &&
+                    passController.text == superpass)))  {
+                  Fluttertoast.showToast(
+                      msg: 'Login successful',
+                      toastLength: Toast.LENGTH_LONG,
+                      gravity: ToastGravity.BOTTOM,
+                      backgroundColor: Colors.yellow,
+                      textColor: Colors.greenAccent);
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyHome()));
+                } else {
+                  Fluttertoast.showToast(
+                      msg: 'Please enter valid ID and Password',
+                      toastLength: Toast.LENGTH_LONG,
+                      gravity: ToastGravity.BOTTOM,
+                      backgroundColor: Colors.yellow,
+                      textColor: Colors.greenAccent);
+                }
+              },
+              child: Text(
+                'Sign In',
+                style: GoogleFonts.rubik(
+                    textStyle: TextStyle(color: Color(0xFFFFFFFF)),
+                    fontSize: 24),
               ),
-              SizedBox(height: 20,),
-              Container(
-                child: TextButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> signup ()));
-                },
-                child: Text('Have an Account? Sing up',style: GoogleFonts.rubik(textStyle: TextStyle(color: Colors.green),fontSize: 15),)
-            ,
-              )
-              )
-            ],
-          )
-      ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+              child: TextButton(
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => signup()));
+            },
+            child: Text(
+              'Have an Account? Sign up',
+              style: GoogleFonts.rubik(
+                  textStyle: TextStyle(color: Colors.green), fontSize: 15),
+            ),
+          ))
+        ],
+      )),
     );
   }
 
-  getSharPref() async{
+  getSharPref() async {
     final pref = await SharedPreferences.getInstance();
 
-    setState((){
-      user = pref.getString('user_name') ?? '' ;
-      pass = pref.getString('pass') ?? '' ;
+    setState(() {
+      user = pref.getString('user_name') ?? '';
+      pass = pref.getString('pass') ?? '';
     });
   }
 }
